33bca20b6d290dc023537488a5ac9edb5fffc3ad
CarfiCadu <carficadu@gmail.com>
2026-08-18 17:41:02 -0300

    feat: criação do EmprestimoService e aplicação das regras de negocio

--- src/main/java/biblioteca/atividade4/repository/EmprestimoRepository.java
+++ src/main/java/biblioteca/atividade4/repository/EmprestimoRepository.java
@@ -4,7 +4,11 @@ import biblioteca.atividade4.Enum.StatusEmprestimo;
 import biblioteca.atividade4.model.Emprestimo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmprestimoRepository extends JpaRepository<Emprestimo, Long> {
     boolean existsByStatusAndUsuarioId(StatusEmprestimo status, Long id);
     boolean existsByStatusAndLivroId(StatusEmprestimo status, Long id);
+    List<Emprestimo> findByUsuarioId(Long id);
+    List<Emprestimo> findByStatus(StatusEmprestimo status);
 }
