1a6d344d225f818826d63fbd36e16e8369ec27c3
CarfiCadu <carficadu@gmail.com>
2026-08-18 17:57:21 -0300

    feat: Criação do UsuarioController, aplicando rotas as suas funcionalidades

---
+++ src/main/java/biblioteca/atividade4/controller/UsuarioController.java
@@ -0,0 +1,53 @@
+package biblioteca.atividade4.controller;
+
+import biblioteca.atividade4.dto.usuario.UsuarioRequestDTO;
+import biblioteca.atividade4.dto.usuario.UsuarioResponseDTO;
+import biblioteca.atividade4.service.UsuarioService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponseDTO> criar(@RequestBody @Valid UsuarioRequestDTO dto) {
+        UsuarioResponseDTO usuario = usuarioService.create(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDTO> buscar(@PathVariable Long id) {
+        UsuarioResponseDTO usuario = usuarioService.findById(id);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponseDTO>> listar() {
+        List<UsuarioResponseDTO> usuario = usuarioService.findAll();
+        return ResponseEntity.ok(usuario);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<UsuarioResponseDTO> atualizar(@PathVariable Long id, @RequestBody @Valid UsuarioRequestDTO dto) {
+        UsuarioResponseDTO usuario = usuarioService.update(id, dto);
+        return ResponseEntity.ok(usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> inativar(@PathVariable Long id) {
+        usuarioService.inativar(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
